529c90a12efe5fa7bfac895be9e020d63d0eb0e5
ricky <125274483+patrickNwafo@users.noreply.github.com>
2024-03-30 16:44:33 +0100

    Update Custormizer.jsx

--- client/src/pages/Custormizer.jsx
+++ client/src/pages/Custormizer.jsx
@@ -57,7 +57,7 @@ const Custormizer = () => {
             // call our backend to generate an ai image!
             setGeneratingImg(true);
 
-            const response = await fetch('http://localhost:8080/api/v1/dalle', {
+            const response = await fetch('https://threejscustomizer.onrender.com/api/v1/dalle', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
